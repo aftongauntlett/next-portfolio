5c80b0378635ef2d2f67b74ef8e28078e29aca13
afton <afton.gauntlett>
2025-05-17 21:45:34 -0400

    theme wip

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,9 @@
-import { type Config } from "tailwindcss";
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      keyframes: {
-        marquee: {
-          "0%": { transform: "translateX(0%)" },
-          "100%": { transform: "translateX(-50%)" },
-        },
-      },
-      animation: {
-        marquee: "marquee 20s linear infinite",
-      },
-    },
+    extend: {},
   },
-  plugins: [],
 };
 
 export default config;
